d4d6fd392aa6375fd4aa5a2fe7fb44007b527754
Alexandr <programalexandr@gmail.com>
2023-12-04 18:04:42 -0500

    adding styles and checkout button

--- src/front-end/App.js
+++ src/front-end/App.js
@@ -2,22 +2,59 @@ import "./App.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import { Carousel } from "primereact/carousel";
 
 function App() {
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
     localStorage.removeItem("token");
+
+    fetch("http://localhost:3001/items")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("fetched data: ", data);
+        setItems(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
+
+  const itemsTemplate = (items) => {
+    return (
+      <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
+        <div className="mb-3">
+          <img src={items.img} alt={items.name} className="w-6 shadow-2" />
+        </div>
+        <div>
+          <h4 className="mb-1">{items.name}</h4>
+          <h6 className="mt-0 mb-3">${items.price}</h6>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="Main-App">
       <Navbar />
       <div className="App">
-        <p>
-          This will be my metroDB website that is hosted on heroku. For now
-          nothing has been setup
-        </p>
+        <Carousel
+          value={items}
+          numVisible={5}
+          numScroll={3}
+          className="custom-carousel"
+          circular
+          autoplayInterval={3000}
+          itemTemplate={itemsTemplate}
+        />
       </div>
     </div>
   );
--- src/front-end/Navbar.js
+++ src/front-end/Navbar.js
@@ -1,4 +1,11 @@
+import "./App.css";
+import { Sidebar } from "primereact/sidebar";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
 export default function Navbar() {
+  const [visible, setVisible] = useState(false);
+
   const backgroundColor = {
     backgroundColor: "transparent",
   };
@@ -16,15 +23,36 @@ export default function Navbar() {
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="/login">Login</a>
-        </li>
-        <li>
           <a href="/sale">On sale</a>
         </li>
         <li>
-          <a href="/checkout">Check Out</a>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <Button style={{ backgroundColor: "transparent", border: 0 }}>
+            <img
+              className="shoppingCart"
+              src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+              alt="Metro Logo"
+              onClick={() => setVisible(true)}
+            />
+          </Button>
         </li>
       </ul>
+      <Sidebar
+        visible={visible}
+        position="right"
+        onHide={() => setVisible(false)}
+      >
+        <h2>Welcome to your cart!</h2>
+        <br />
+        <p>
+          The cart is currently under construction. But our developers are
+          working hard on finishing it for your use.
+        </p>
+        <br />
+        <p>Please follow us on our socials to keep up with our news!</p>
+      </Sidebar>
     </nav>
   );
 }
